941e268c48b1b6098ddcf7ad58005e026ebba4bc
resakse <resakse@gmail.com>
2026-01-24 19:19:02 +0800

    fix compile error in obj_from_char null check

--- src/handler.c
+++ src/handler.c
@@ -1625,9 +1625,9 @@ void obj_from_char(P_obj object)
     for (tmp = object->loc.carrying->carrying; tmp && (tmp->next_content != object); tmp = tmp->next_content) ; /* locate previous */
 
     if (!tmp) {
-      logit(LOG_BUG, "obj_from_char: object %s (%d) not in carrying list of %s",
+      logit(LOG_DEBUG, "obj_from_char: object %s (%d) not in carrying list of %s",
             object->short_description ? object->short_description : "unknown",
-            GET_OBJ_VNUM(object),
+            OBJ_VNUM(object),
             object->loc.carrying->player.name ? object->loc.carrying->player.name : "unknown");
       return;
     }
