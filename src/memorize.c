f5f1d074ef0a6e1728fe6e1606c3c043b1d3361c
Adam Borowski <kilobyte@angband.pl>
2026-06-03 21:46:13 +0200

    QoL: mages can partially mem if they lack some books.
    It's infuriating to have to forget everything just to mem minor c to
    scribe the spells.

--- src/memorize.c
+++ src/memorize.c
@@ -1070,6 +1070,7 @@ void handle_memorize(P_char ch)
 {
 	struct affected_type *af;
 	bool                  memorized = FALSE;
+	bool                  no_book = FALSE;
 	int                   time;
 
 	if (!IS_AFFECTED2(ch, AFF2_MEMORIZING))
@@ -1087,20 +1088,24 @@ void handle_memorize(P_char ch)
 		{
 			if (memorized)
 			{
+#if !defined(CHAOS_MUD) || (CHAOS_MUD != 1)
+				if (book_class(ch) && !(SpellInSpellBook(ch, af->modifier, SBOOK_MODE_IN_INV | SBOOK_MODE_AT_HAND | SBOOK_MODE_ON_BELT | SBOOK_MODE_ON_GROUND)))
+				{
+					no_book = true;
+					continue;
+				}
+#endif
 				time = get_circle_memtime(ch, get_spell_circle(ch, af->modifier));
 				add_event(event_memorize, time / 2, ch, 0, 0, 0, &time, sizeof(time));
 				return;
 			}
 			else
-			{
 #if !defined(CHAOS_MUD) || (CHAOS_MUD != 1)
 				if (book_class(ch) && !(SpellInSpellBook(ch, af->modifier, SBOOK_MODE_IN_INV | SBOOK_MODE_AT_HAND | SBOOK_MODE_ON_BELT | SBOOK_MODE_ON_GROUND)))
-				{
-					send_to_char("You have managed to misplace your spellbook!\n", ch);
-					show_stop_memorizing(ch);
-					return;
-				}
+					no_book = TRUE;
+			else
 #endif
+			{
 				if (meming_class(ch))
 				{
 					snprintf(Gbuf1, MAX_STRING_LENGTH, "You have finished memorizing %s.\n", skills[af->modifier].name);
@@ -1114,6 +1119,13 @@ void handle_memorize(P_char ch)
 				memorized = TRUE;
 			}
 		}
+	}
+
+	if (no_book)
+	{
+		send_to_char("You have managed to misplace your spellbook!\n", ch);
+		show_stop_memorizing(ch);
+		return;
 	}
 
 	if (GET_CLASS(ch, CLASS_SHAMAN))
